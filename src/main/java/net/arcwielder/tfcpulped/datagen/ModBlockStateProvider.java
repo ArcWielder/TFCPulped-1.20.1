0fd3e5c4752ab10ea211a808d67e0394d3c6dd4d
ArcWielder <aarenruparel@gmail.com>
2024-07-15 13:53:47 -0700

    TFC Pulped

---
+++ src/main/java/net/arcwielder/tfcpulped/datagen/ModBlockStateProvider.java
@@ -0,0 +1,20 @@
+package net.arcwielder.tfcpulped.datagen;
+
+import net.arcwielder.tfcpulped.TFCPulped;
+import net.arcwielder.tfcpulped.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, TFCPulped.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        simpleBlock(ModBlocks.DRYING_BASIN.get(),
+                new ModelFile.UncheckedModelFile(modLoc("block/drying_basin")));
+    }
+}
