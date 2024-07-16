b8faaae9774c357f7b49275c211fb201e458dd19
ArcWielder <aarenruparel@gmail.com>
2024-07-16 13:19:17 -0700

    TFC Pulped

---
+++ src/main/java/net/arcwielder/tfcpulped/event/ModEventBusClientEvents.java
@@ -0,0 +1,22 @@
+package net.arcwielder.tfcpulped.event;
+
+import net.arcwielder.tfcpulped.TFCPulped;
+import net.arcwielder.tfcpulped.block.entity.ModBlockEntities;
+import net.arcwielder.tfcpulped.block.entity.renderer.DryingBasinBlockEntityRenderer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TFCPulped.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ModEventBusClientEvents {
+    @SubscribeEvent
+    public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
+
+    }
+
+    @SubscribeEvent
+    public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(ModBlockEntities.DRYING_BASIN_BE.get(), DryingBasinBlockEntityRenderer::new);
+    }
+}
